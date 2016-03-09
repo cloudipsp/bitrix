2b81f001714554f9199656a57710cf1708390148
dmiroshnikov <dmmatrix222@gmail.com>
2016-03-09 09:50:27 +0200

    1

--- fondy/ru/.description.php
+++ fondy/ru/.description.php
@@ -7,7 +7,7 @@ $MESS["OPLATA_DESC_PRICE_CURRENCY"] = "<b style='color=#aeaeae;'>Внимание!</b> Э
 $MESS["OPLATA_RESPONSE_URL"] = "Ссылка возврата клиента";
 $MESS["OPLATA_DESC_RESPONSE_URL"] = "Ссылка, на которую вернется клиент после завершения платежа. <br>Например http://{yourdomain}/personal/order/";
 $MESS["OPLATA_SERVER_CALLBACK_URL"] = "Ссылка ответа платежной системы";
-$MESS["OPLATA_DESC_SERVER_CALLBACK_URL"] = "Ссылка, на которую зайдет платежная система для завершения заказа.<br>Например http://{yourdomain}/bitrix/tools/oplata_result.php";
+$MESS["OPLATA_DESC_SERVER_CALLBACK_URL"] = "Ссылка, на которую зайдет платежная система для завершения заказа.<br>Например http://{yourdomain}/bitrix/tools/fondy_result.php";
 $MESS["OPLATA_LANGUAGE"] = "Язык страницы платежной системы";
 $MESS["OPLATA_DESC_LANGUAGE"] = "Например : RU";
 
