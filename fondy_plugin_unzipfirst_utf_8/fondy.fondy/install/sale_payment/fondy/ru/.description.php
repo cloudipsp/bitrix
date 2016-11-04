0ec7a947ad2f9a79462130cb52a71030fade0f3a
dmiroshnikov <dmmatrix222@gmail.com>
2016-11-04 11:47:27 +0200

    add utf-8

---
+++ fondy_plugin_unzipfirst_utf_8/fondy.fondy/install/sale_payment/fondy/ru/.description.php
@@ -0,0 +1,15 @@
+<?
+global $MESS;
+$MESS["FONDY_MERCHANT"] = "Идентификатор мерчанта";
+$MESS["FONDY_SECURE_KEY"] = "Секретный ключ";
+$MESS["FONDY_PRICE_CURRENCY"] = "Валюта платежа";
+$MESS["FONDY_DESC_PRICE_CURRENCY"] = "<b style='color=#aeaeae;'>Внимание!</b> Это значение должно соответствовать валюте вашего мерчанта";
+$MESS["FONDY_RESPONSE_URL"] = "Ссылка возврата клиента";
+$MESS["FONDY_DESC_RESPONSE_URL"] = "Ссылка, на которую вернется клиент после завершения платежа. <br>Например http://{yourdomain}/personal/order/";
+$MESS["FONDY_SERVER_CALLBACK_URL"] = "Ссылка ответа платежной системы";
+$MESS["FONDY_DESC_SERVER_CALLBACK_URL"] = "Ссылка, на которую зайдет платежная система для завершения заказа.<br>Например http://{yourdomain}/bitrix/tools/fondy_result/fondy_result.php";
+$MESS["FONDY_LANGUAGE"] = "Язык страницы платежной системы";
+$MESS["FONDY_DESC_LANGUAGE"] = "Например : RU";
+$MESS["FONDY_AMOUNT"] = "Сумма для списания при оплате в магазине";
+$MESS["FONDY_ONPAGE"] = "Включить режим оплаты на странице магазина";
+?>
---
+++ fondy_plugin_unzipfirst_win_1251/fondy.fondy/install/index.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Fondy Payment Module
+	*
+ * NOTICE OF LICENSE
+	*
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+	*
+ * @category        Fondy
+ * @package         fondy.fondy
+ * @version         1.0.0
+ * @author          Fondy
+ * @copyright       Copyright (c) 2016 Fondy
+ * @license         http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+	*
+ * EXTENSION INFORMATION
+	*
+ * 1C-Bitrix        16.0
+ * Fondy API       https://fondy.eu
+	*
+ */
+
+IncludeModuleLangFile(__FILE__);
+
+class fondy_fondy extends CModule
+{
+
+    const MODULE_ID = 'fondy.fondy';
+    const PARTNER_NAME = 'DM';
+    const PARTNER_URI = 'https://fondy.eu';
+
+    var $MODULE_ID = 'fondy.fondy';
+    var $PARTNER_NAME = 'DM';
+    var $PARTNER_URI = 'https://fondy.eu';
+
+    public $MODULE_GROUP_RIGHTS = 'N';
+
+    public function __construct()
+    {
+        require(dirname(__FILE__).'/version.php');
+        $this->MODULE_NAME = GetMessage('F_MODULE_NAME');
+        $this->MODULE_DESCRIPTION = GetMessage('F_MODULE_DESC');
+        $this->MODULE_VERSION = $arModuleVersion['VERSION'];
+        $this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
+        $this->PARTNER_NAME = 'DM';
+        $this->PARTNER_URI = 'http://fondy.eu';
+    }
+
+    public function DoInstall()
+    {
+        if (IsModuleInstalled('sale')) {
+            global $APPLICATION;
+            $this->InstallFiles();
+            RegisterModule($this->MODULE_ID);
+            return true;
+        }
+
+        $MODULE_ID = $this->MODULE_ID;
+        $TAG = 'VWS';
+        $MESSAGE = GetMessage('F_ERR_MODULE_NOT_FOUND', array('#MODULE#'=>'sale'));
+        $intID = CAdminNotify::Add(compact('MODULE_ID', 'TAG', 'MESSAGE'));
+
+        return false;
+    }
+
+    public function DoUninstall()
+    {
+        global $APPLICATION;
+        COption::RemoveOption($this->MODULE_ID);
+        UnRegisterModule($this->MODULE_ID);
+        $this->UnInstallFiles();
+    }
+
+    public function InstallFiles()
+    {
+        CopyDirFiles(
+            $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/'.$this->MODULE_ID.'/install/sale_payment/fondy_result',
+            $_SERVER['DOCUMENT_ROOT'].'/bitrix/tools/fondy_result',
+            true, true
+        );
+		CopyDirFiles(
+			$_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/'.$this->MODULE_ID.'/install/sale_payment',
+			$_SERVER['DOCUMENT_ROOT'].'/bitrix/php_interface/include/sale_payment',
+		true, true
+        );
+    }
+
+    public function UnInstallFiles()
+    {
+		DeleteDirFilesEx("/bitrix/php_interface/include/sale_payment/fondy");
+		DeleteDirFilesEx("/bitrix/tools/fondy_result");
+        return true;
+    }
+}
