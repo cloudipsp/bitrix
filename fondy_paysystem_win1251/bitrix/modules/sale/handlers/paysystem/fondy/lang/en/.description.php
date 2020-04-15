9a7d1963d3af95c6592c39bf86cf51ac8b26d1d8
Andrey Sydorenko <dev.sidorenko@gmail.com>
2020-04-15 09:17:10 +0300

    Added translate to en and pl

---
+++ fondy_paysystem_win1251/bitrix/modules/sale/handlers/paysystem/fondy/lang/en/.description.php
@@ -0,0 +1,18 @@
+<?
+global $MESS;
+$MESS['FONDY_ORDER_ID'] = 'Unique purchase ID in your store';
+$MESS["FONDY_MERCHANT"] = "Merchant ID";
+$MESS["FONDY_SECURE_KEY"] = "Secret Key";
+$MESS["SALE_HANDLERS_PAY_SYSTEM_FONDY_BUTTON_PAID"] = "FONDY Pay";
+$MESS["FONDY_PRICE_CURRENCY"] = "Currency";
+$MESS["FONDY_DESC_PRICE_CURRENCY"] = "Attention! This value must match the currency of your merchant";
+$MESS["FONDY_RESPONSE_URL"] = "Customer Return Link";
+$MESS["FONDY_DESC_RESPONSE_URL"] = "Link, where client will be redirected to after successful payment. <br>Example: http://{yourdomain}/personal/order/";
+$MESS["FONDY_SERVER_CALLBACK_URL"] = "Payment platform response link";
+$MESS["FONDY_DESC_SERVER_CALLBACK_URL"] = "The link to be followed by the payment platform to complete the order. <br>Example: http://{yourdomain}/bitrix/tools/fondy_result/fondy_result.php";
+$MESS["FONDY_LANGUAGE"] = "Language of the payment platform page";
+$MESS["FONDY_DESC_LANGUAGE"] = "Example: EN";
+$MESS["FONDY_AMOUNT"] = "Amount to be debited";
+$MESS["FONDY_ONPAGE"] = "Enable payment mode on the store page";
+$MESS["FONDY_PREAUTH"] = "Payment holding (Parameter is applied only when paying with Visa / MasterCard)";
+?>
---
+++ fondy_plugin_unzipfirst_win1251/fondy.fondy/lang/pl/install/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Fondy Payment Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category        Fondy
+ * @package         fondy.fondy
+ * @version         1.0.0
+ * @author          Fondy
+ * @copyright       Copyright (c) 2016 Fondy
+ * @license         http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ *
+ * EXTENSION INFORMATION
+ *
+ * 1C-Bitrix        16.0
+ * Fondy API       https://fondy.eu
+ *
+ */
+
+$MESS['F_MODULE_NAME'] = 'FONDY platforma p?atno?ci';
+$MESS['F_MODULE_DESC'] = 'Obs?uga platfromy p?atno?ci FONDY';
+$MESS['F_ERR_MODULE_NOT_FOUND'] = '#MODULE# jest wymagany, aby zainstalowa? to rozwi?zanie.';
